8d96675506dea0791472103a3f479d116fca91b6
Simon Berger <berger@i6.informatik.rwth-aachen.de>
2023-06-05 16:33:15 +0200

    Better setup script

--- apptainer/2023-05-08_tensorflow-2.8_v1/setup_makefiles.sh
+++ apptainer/2023-05-08_tensorflow-2.8_v1/setup_makefiles.sh
@@ -3,8 +3,11 @@ SCRIPT_DIR=$(dirname -- "$0")
 BASE_DIR=$SCRIPT_DIR/../..
 MAKEFILES_DIR=$SCRIPT_DIR/makefiles
 
-files=("Makefile" "Config.make" "Modules.make" "Options.make" "Rules.make" "config/cc-gcc.make" "config/os-linux.make" "config/proc-aarch64.make" "config/proc-x86_64.make")
+files=("Makefile" "Makefile.cfg" "Config.make" "Modules.make" "Options.make" "Rules.make" "config/cc-clang.make" "config/cc-gcc.make" "config/cc-icc.make" "config/os-linux.make" "config/os-darwin.make" "config/proc-aarch64.make" "config/proc-x86_64.make")
 
 for file in "${files[@]}"; do
+    echo "Copy $file..."
     cp $MAKEFILES_DIR/$file $BASE_DIR/$file
 done
+
+echo "Finished setup."

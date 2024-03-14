26adda8aea489f097a7a838a21c8b48a61c55d96
Simon Berger <berger@i6.informatik.rwth-aachen.de>
2024-03-14 16:40:57 +0100

    CMake compile script (specific to i6)

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p cmake-build-standard
+pushd cmake-build-standard || exit
+apptainer exec -B /u/berger /work/asr4/berger/apptainer/images/i6_tensorflow-2.8_onnx-1.15.sif /u/berger/tools/clion-2023.2.2/bin/cmake/linux/x64/bin/cmake .. -DCMAKE_BUILD_TYPE=standard -G Ninja -DCMAKE_MAKE_PROGRAM=/u/berger/tools/clion-2023.2.2/bin/ninja/linux/x64/ninja
+apptainer exec -B /u/berger /work/asr4/berger/apptainer/images/i6_tensorflow-2.8_onnx-1.15.sif /u/berger/tools/clion-2023.2.2/bin/cmake/linux/x64/bin/cmake --build . --target all -j 10
+apptainer exec -B /u/berger /work/asr4/berger/apptainer/images/i6_tensorflow-2.8_onnx-1.15.sif /u/berger/tools/clion-2023.2.2/bin/cmake/linux/x64/bin/cmake --install .
+popd || exit

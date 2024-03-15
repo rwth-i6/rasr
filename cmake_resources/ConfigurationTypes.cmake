fc5f2f3644c18625fee2bf1ed07b7009040a7ef5
Simon Berger <berger@i6.informatik.rwth-aachen.de>
2024-03-15 10:44:05 +0100

    CMakeLists formatting

--- cmake_resources/ConfigurationTypes.cmake
+++ cmake_resources/ConfigurationTypes.cmake
@@ -1,8 +1,11 @@
-set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "standard" "debug" "release")
+set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "standard" "debug"
+                                             "release")
 
-if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-    message(STATUS "Setting build type to 'standard' as none was specified.")
-    set(CMAKE_BUILD_TYPE standard CACHE STRING "Choose the type of build." FORCE)
+if(NOT CMAKE_BUILD_TYPE)
+  message(STATUS "Setting build type to 'standard' as none was specified.")
+  set(CMAKE_BUILD_TYPE
+      standard
+      CACHE STRING "Choose the type of build." FORCE)
 endif()
 
 set(CMAKE_C_FLAGS_STANDARD "-O2")
--- cmake_resources/UniversalSettings.cmake
+++ cmake_resources/UniversalSettings.cmake
@@ -4,29 +4,30 @@ add_compile_definitions(CMAKE_DISABLE_MODULES_HH)
 
 add_compile_definitions(_GNU_SOURCE)
 add_compile_options(
-        -pipe
-        -funsigned-char
-        -fno-exceptions
-        -Wno-unknown-pragmas
-        -Wall
-        -Wno-long-long
-        -Wno-deprecated
-        -fno-strict-aliasing
-        -ffast-math
-        -msse3
-)
+  -pipe
+  -funsigned-char
+  -fno-exceptions
+  -Wno-unknown-pragmas
+  -Wall
+  -Wno-long-long
+  -Wno-deprecated
+  -fno-strict-aliasing
+  -ffast-math
+  -msse3)
 
-if (NOT DEFINED MARCH)
-    set(MARCH "native" CACHE STRING "Default target architecture")
-endif ()
+if(NOT DEFINED MARCH)
+  set(MARCH
+      "native"
+      CACHE STRING "Default target architecture")
+endif()
 include(CheckCXXCompilerFlag)
-CHECK_CXX_COMPILER_FLAG("-march=${MARCH}" COMPILER_SUPPORTS_MARCH)
-if (COMPILER_SUPPORTS_MARCH)
-    add_compile_options("-march=${MARCH}")
-    message(STATUS "Using -march=${MARCH}")
-else ()
-    message(STATUS "Architecture -march=${MARCH} is not supported")
-endif ()
+check_cxx_compiler_flag("-march=${MARCH}" COMPILER_SUPPORTS_MARCH)
+if(COMPILER_SUPPORTS_MARCH)
+  add_compile_options("-march=${MARCH}")
+  message(STATUS "Using -march=${MARCH}")
+else()
+  message(STATUS "Architecture -march=${MARCH} is not supported")
+endif()
 
 find_package(LibXml2 REQUIRED)
 find_package(Threads REQUIRED)
@@ -35,46 +36,41 @@ find_package(LAPACK REQUIRED)
 
 find_library(LIB_RT rt REQUIRED)
 
-link_libraries(LibXml2::LibXml2 ZLIB::ZLIB Threads::Threads LAPACK::LAPACK ${LIB_RT})
+link_libraries(LibXml2::LibXml2 ZLIB::ZLIB Threads::Threads LAPACK::LAPACK
+               ${LIB_RT})
 
 include_directories(${LIBXML2_INCLUDE_DIR})
 include_directories(${CMAKE_SOURCE_DIR}/src)
 
-if (${MODULE_AUDIO_FFMPEG})
-    find_package(PkgConfig REQUIRED)
-    pkg_check_modules(AVFORMAT REQUIRED IMPORTED_TARGET libavformat)
-    pkg_check_modules(SWRESAMPLE REQUIRED IMPORTED_TARGET libswresample)
-    pkg_check_modules(AVCODEC REQUIRED IMPORTED_TARGET libavcodec)
-    pkg_check_modules(AVUTIL REQUIRED IMPORTED_TARGET libavutil)
+if(${MODULE_AUDIO_FFMPEG})
+  find_package(PkgConfig REQUIRED)
+  pkg_check_modules(AVFORMAT REQUIRED IMPORTED_TARGET libavformat)
+  pkg_check_modules(SWRESAMPLE REQUIRED IMPORTED_TARGET libswresample)
+  pkg_check_modules(AVCODEC REQUIRED IMPORTED_TARGET libavcodec)
+  pkg_check_modules(AVUTIL REQUIRED IMPORTED_TARGET libavutil)
 
-    include_directories(${AVFORMAT_INCLUDE_DIRS} ${SWRESAMPLE_INCLUDE_DIRS} ${AVCODEC_INCLUDE_DIRS} ${AVUTIL_INCLUDE_DIRS})
-    link_libraries(PkgConfig::AVFORMAT PkgConfig::SWRESAMPLE PkgConfig::AVCODEC PkgConfig::AVUTIL)
-endif ()
-
-if (${MODULE_OPENMP})
-    find_package(OpenMP REQUIRED)
-    add_compile_options(OpenMP_CXX_FLAGS)
-    include_directories(OpenMP_CXX_INCLUDE_DIRS)
-    link_libraries(OpenMP::OpenMP_CXX)
-endif ()
-
-if (${MODULE_TENSORFLOW})
-    include(cmake_resources/Tensorflow.cmake)
-endif ()
-
-if(${MODULE_CUDA})
-    enable_language(CUDA)
-    set(CMAKE_CUDA_ARCHITECTURES 61;75;86)
+  include_directories(${AVFORMAT_INCLUDE_DIRS} ${SWRESAMPLE_INCLUDE_DIRS}
+                      ${AVCODEC_INCLUDE_DIRS} ${AVUTIL_INCLUDE_DIRS})
+  link_libraries(PkgConfig::AVFORMAT PkgConfig::SWRESAMPLE PkgConfig::AVCODEC
+                 PkgConfig::AVUTIL)
 endif()
 
-if (${MODULE_AUDIO_FLAC})
-    link_libraries(FLAC)
-endif ()
+if(${MODULE_OPENMP})
+  find_package(OpenMP REQUIRED)
+  add_compile_options(OpenMP_CXX_FLAGS)
+  include_directories(OpenMP_CXX_INCLUDE_DIRS)
+  link_libraries(OpenMP::OpenMP_CXX)
+endif()
 
-if (${MODULE_AUDIO_WAV_SYSTEM})
-    link_libraries(sndfile)
-endif ()
+if(${MODULE_CUDA})
+  enable_language(CUDA)
+  set(CMAKE_CUDA_ARCHITECTURES 61;75;86;89)
+endif()
 
-if (${MODULE_PYTHON})
-    include(cmake_resources/Python.cmake)
-endif ()
+if(${MODULE_AUDIO_FLAC})
+  link_libraries(FLAC)
+endif()
+
+if(${MODULE_AUDIO_WAV_SYSTEM})
+  link_libraries(sndfile)
+endif()

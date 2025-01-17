3dad9210e9ac06548e19fa935f1dfb214f3784b9
Simon Berger <berger@i6.informatik.rwth-aachen.de>
2025-01-17 13:48:07 +0100

    Change year to 2025

--- src/Nn/LabelScorer/BufferedLabelScorer.cc
+++ src/Nn/LabelScorer/BufferedLabelScorer.cc
@@ -1,4 +1,4 @@
-/** Copyright 2024 RWTH Aachen University. All rights reserved.
+/** Copyright 2025 RWTH Aachen University. All rights reserved.
  *
  *  Licensed under the RWTH ASR License (the "License");
  *  you may not use this file except in compliance with the License.

c66f3fd4b6ac16b58ead3bda7f16d368943394ea
Simon Berger <berger@i6.informatik.rwth-aachen.de>
2025-02-06 16:22:31 +0100

    Implement StepwiseNoOpLabelScorer

---
+++ src/Nn/LabelScorer/NoOpLabelScorer.hh
@@ -0,0 +1,45 @@
+/** Copyright 2025 RWTH Aachen University. All rights reserved.
+ *
+ *  Licensed under the RWTH ASR License (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.hltpr.rwth-aachen.de/rwth-asr/rwth-asr-license.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef NO_OP_LABEL_SCORER_HH
+#define NO_OP_LABEL_SCORER_HH
+
+#include "BufferedLabelScorer.hh"
+
+namespace Nn {
+
+/*
+ * Label Scorer that performs no computation internally. It assumes that the input features are already
+ * finished score vectors and just returns the score at the current time step.
+ */
+class StepwiseNoOpLabelScorer : public BufferedLabelScorer {
+    using Precursor = BufferedLabelScorer;
+
+public:
+    StepwiseNoOpLabelScorer(const Core::Configuration& config);
+
+    // Initial scoring context just contains step 0.
+    ScoringContextRef getInitialScoringContext() override;
+
+    // Scoring context with step incremented by 1.
+    virtual ScoringContextRef extendedScoringContext(LabelScorer::Request const& request) override;
+
+    // Gets the buffered score for the requested token at the requested step
+    std::optional<LabelScorer::ScoreWithTime> computeScoreWithTime(LabelScorer::Request const& request) override;
+};
+
+}  // namespace Nn
+
+#endif  // NO_OP_LABEL_SCORER_HH

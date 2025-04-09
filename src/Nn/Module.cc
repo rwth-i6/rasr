b98ccf763b412961b2071961da0ea379d6956b6b
Simon Berger <simon.berger95@gmail.com>
2025-04-09 18:34:19 +0200

    Add LimitedCtxOnnxLabelScorer

--- src/Nn/Module.cc
+++ src/Nn/Module.cc
@@ -20,6 +20,7 @@
 
 #include "LabelScorer/CombineLabelScorer.hh"
 #include "LabelScorer/EncoderDecoderLabelScorer.hh"
+#include "LabelScorer/LimitedCtxOnnxLabelScorer.hh"
 #include "LabelScorer/NoCtxOnnxLabelScorer.hh"
 #include "LabelScorer/NoOpLabelScorer.hh"
 #include "Statistics.hh"
@@ -111,6 +112,13 @@ Module_::Module_()
             "no-ctx-onnx",
             [](Core::Configuration const& config) {
                 return Core::ref(new NoCtxOnnxLabelScorer(config));
+            });
+
+    // Compute scores by forwarding a single input feature vector together with a fixed-size history through an ONNX model
+    labelScorerFactory_.registerLabelScorer(
+            "limited-ctx-onnx",
+            [](Core::Configuration const& config) {
+                return Core::ref(new LimitedCtxOnnxLabelScorer(config));
             });
 };
 

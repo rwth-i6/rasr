ecb9199308ccc29ea14a6d387186ce0c084e6a8c
Eugen Beck <ebeck@apptek.com>
2025-04-24 12:00:08 -0400

    Revert "More"
    This reverts commit 8dfef94913dc79c2e396fa3b4ea84f26472e4d3e.

--- src/Bliss/LexiconParser.cc
+++ src/Bliss/LexiconParser.cc
@@ -262,11 +262,7 @@ void LexiconElement::addPhon(const WeightedPhonemeString& phon) {
     }
 
     Pronunciation* pron = product_->getPronunciation(phon.phon);
-    if (pron == nullptr) {
-        parser()->error("Could not create pronunciation for %s", phon.phon);
-        return;
-    }
-    else if (lemma_->hasPronunciation(pron)) {
+    if (lemma_->hasPronunciation(pron)) {
         parser()->error("duplicate pronunciation");
         return;
     }

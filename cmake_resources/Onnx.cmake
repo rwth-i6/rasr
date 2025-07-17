68b6c34f324ed57ae77f27d8ca2c241fa08c4944
Simon Berger <simon.berger95@gmail.com>
2025-07-17 15:21:13 +0200

    Simplify dependencies in CMakeLists files + formatting

--- cmake_resources/Onnx.cmake
+++ cmake_resources/Onnx.cmake
@@ -1,9 +1,8 @@
 set(onnx_INCLUDE_DIR "/opt/thirdparty/usr/include")
 
-find_library(onnxruntime_LIBRARY onnxruntime REQUIRED
-             HINTS "/opt/thirdparty/usr/lib")
+find_library(onnxruntime_LIBRARY onnxruntime REQUIRED HINTS "/opt/thirdparty/usr/lib")
 
 function(add_onnx_dependencies TARGET)
-  target_include_directories(${TARGET} PUBLIC ${onnx_INCLUDE_DIR})
-  target_link_libraries(${TARGET} PUBLIC ${onnxruntime_LIBRARY})
+    target_include_directories(${TARGET} PUBLIC ${onnx_INCLUDE_DIR})
+    target_link_libraries(${TARGET} PUBLIC ${onnxruntime_LIBRARY})
 endfunction()

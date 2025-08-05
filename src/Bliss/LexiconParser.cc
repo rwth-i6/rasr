800638f9e3b0a101ef976bc726ab8b9dc372bd92
Eugen Beck <ebeck@apptek.com>
2025-08-05 08:26:09 -0400

    Fixes

--- src/Bliss/LexiconParser.cc
+++ src/Bliss/LexiconParser.cc
@@ -463,8 +463,14 @@ void VocabTextLexiconParser::createLemmata() {
         Lemma* newLemma_ = lexicon_->newLemma();
         // set orth
         lexicon_->setOrthographicForms(newLemma_, {symbol});
+
         // set phon
-        Pronunciation* pron = lexicon_->getPronunciation(symbol);
+        Pronunciation* pron   = nullptr;
+        Core::Status   status = lexicon_->getPronunciation(symbol, pron);
+        if (pron == nullptr) {
+            Core::Application::us()->error("Could not create pronunciation for %s: %s", symbol.c_str(), status.message().c_str());
+            return;
+        }
         lexicon_->addPronunciation(newLemma_, pron);
         lexicon_->setDefaultLemmaName(newLemma_);
     }

2f7a5b860df939287ffe1635f763fb8dfc0b706e
Simon Berger <simon.berger95@gmail.com>
2025-11-24 10:39:46 +0100

    Include src/Tools/LibRASR/Lexicon.cc in CMakeLists.txt

--- compile.sh
+++ compile.sh
@@ -21,6 +21,7 @@ run_in_apptainer() {
         "$@"
 }
 
-run_in_apptainer cmake -S . -B "${BUILD_DIR}" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DMODULE_TENSORFLOW=0 -DMODULE_LM_TFRNN=0
+run_in_apptainer cmake -S . -B "${BUILD_DIR}" -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DMODULE_TENSORFLOW=0 -DMODULE_LM_TFRNN=0
+mv "${BUILD_DIR}/compile_commands.json" .
 run_in_apptainer cmake --build "${BUILD_DIR}" --parallel
 run_in_apptainer cmake --install "${BUILD_DIR}"

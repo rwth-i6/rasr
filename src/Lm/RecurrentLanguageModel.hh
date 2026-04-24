ee649782b8ccac77b64d7c12316a67c2f81ba7c2
SimBe195 <37951951+SimBe195@users.noreply.github.com>
2026-04-24 14:06:45 +0200

    Fix RecurrentLanguageModel: add missing output_transform_function if no transformation should be performed (#211)

--- src/Lm/RecurrentLanguageModel.hh
+++ src/Lm/RecurrentLanguageModel.hh
@@ -1,6 +1,7 @@
 #ifndef _LM_RECURRENT_LANGUAGE_MODEL_HH
 #define _LM_RECURRENT_LANGUAGE_MODEL_HH
 
+#include <functional>
 #include <future>
 #include <thread>
 #include <vector>
@@ -312,6 +313,9 @@ RecurrentLanguageModel<value_t, state_variable_t>::RecurrentLanguageModel(Core::
         output_transform_function_ = [](Score v) {
             return -v;
         };
+    }
+    else {
+        output_transform_function_ = std::identity();
     }
 
     if (async_) {

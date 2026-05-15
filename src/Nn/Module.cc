3d5b2e474bf7808a49fbecc454f219ce0a978efb
SimBe195 <37951951+SimBe195@users.noreply.github.com>
2026-05-15 11:51:48 +0200

    Add CtcPrefixLabelScorer (#205)

--- src/Nn/Module.cc
+++ src/Nn/Module.cc
@@ -17,6 +17,7 @@
 #include <Core/FormatSet.hh>
 #include <Flow/Registry.hh>
 #include "LabelScorer/CombineLabelScorer.hh"
+#include "LabelScorer/CtcPrefixLabelScorer.hh"
 #include "LabelScorer/EncoderDecoderLabelScorer.hh"
 #include "LabelScorer/FixedContextOnnxLabelScorer.hh"
 #include "LabelScorer/NoContextOnnxLabelScorer.hh"
@@ -80,6 +81,13 @@ Module_::Module_()
             "combine",
             [](Core::Configuration const& config) {
                 return Core::ref(new CombineLabelScorer(config));
+            });
+
+    // A label scorer that wraps a time-synchronous CTC scorer and computes label-synchronous prefix scores
+    labelScorerFactory_.registerLabelScorer(
+            "ctc-prefix",
+            [](Core::Configuration const& config) {
+                return Core::ref(new CtcPrefixLabelScorer(config));
             });
 
     // Assumes inputs are already finished scores and just passes on the score at the current step
